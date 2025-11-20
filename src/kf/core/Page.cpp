fd0315a10ff16eacfba1387e4fb3fb650f854d5b
JamahaW <153020210+JamahaW@users.noreply.github.com>
2025-11-20 17:57:21 +0300

    Move some Page methods implementations in Page.cpp PageSetterButton -> Page::PageSetter (now private)

--- src/kf/core/Page.cpp
+++ src/kf/core/Page.cpp
@@ -2,13 +2,57 @@
 #include <kf/core/PageManager.hpp>
 
 
-void kf::ui::PageSetterButton::doRender(Render &render) const {
+void kf::ui::Page::render(kf::ui::Render &render, int rows) {
+    render.print(title);
+    render.write('\n');
+
+    rows -= 1;
+
+    const auto start = (totalWidgets() > rows) ? std::min(cursor, totalWidgets() - rows) : 0;
+    const auto end = std::min(start + rows, totalWidgets());
+
+    for (int i = start; i < end; i += 1) {
+        widgets[i]->render(render, i == cursor);
+        render.write('\n');
+    }
+}
+
+bool kf::ui::Page::onEvent(kf::ui::Event event) {
+    switch (event) {
+        case Event::None: {
+            return false;
+        }
+        case Event::Update: {
+            return true;
+        }
+        case Event::ElementNext: {
+            moveCursor(+1);
+            return true;
+        }
+        case Event::ElementPrevious: {
+            moveCursor(-1);
+            return true;
+        }
+        case Event::Click: {
+            return widgets[cursor]->onClick();
+        }
+        case Event::ChangeIncrement: {
+            return widgets[cursor]->onChange(+1);
+        }
+        case Event::ChangeDecrement: {
+            return widgets[cursor]->onChange(-1);
+        }
+    }
+    return false;
+}
+
+void kf::ui::Page::PageSetter::doRender(Render &render) const {
     render.write('>');
     render.write(' ');
     render.print(target.title);
 }
 
-bool kf::ui::PageSetterButton::onClick() {
+bool kf::ui::Page::PageSetter::onClick() {
     PageManager::instance().bind(target);
     return true;
 }
--- src/kf/core/Page.hpp
+++ src/kf/core/Page.hpp
@@ -11,41 +11,40 @@
 
 namespace kf::ui {
 
-/// @brief Страница, содержит виджеты. Имеет заголовок.
-struct Page;
-
-/// @brief Специальный виджет для создания кнопки перехода на страницу
-struct PageSetterButton final : Widget {
-
-    /// @brief Страница перехода
-    Page &target;
-
-    explicit PageSetterButton(Page &target) :
-        target{target} {}
-
-    /// @brief Устанавливает активную страницу
-    bool onClick() override;
-
-protected:
-
-    void doRender(Render &render) const override;
-};
-
+/// @brief Страница, содержит виджеты и имеет заголовок.
 struct Page {
 
-    /// @brief Заголовок страницы.
-    const char *title;
-
 private:
+
+    /// @brief Специальный виджет для создания кнопки перехода на страницу
+    /// @note Не используется в пользовательском коде. Для связывания страниц используйте <code>kf::ui::Page::link</code>
+    struct PageSetter final : Widget {
+
+        /// @brief Страница перехода
+        Page &target;
+
+        explicit PageSetter(Page &target) :
+            target{target} {}
+
+        /// @brief Устанавливает активную страницу
+        bool onClick() override;
+
+    protected:
+
+        void doRender(Render &render) const override;
+    };
 
     /// @brief Виджеты данной страницы
     std::vector<Widget *> widgets{};
 
-    /// @brief Курсор (Активный виджет)
+    /// @brief Заголовок страницы.
+    const char *title;
+
+    /// @brief Курсор (Индекс активного виджета)
     int cursor{0};
 
     /// @brief Виджет перехода к данной странице
-    PageSetterButton to_this{*this};
+    PageSetter to_this{*this};
 
 public:
     explicit Page(const char *title) noexcept:
@@ -53,66 +52,26 @@ public:
 
     /// @brief Добавить виджет в данную страницу
     /// @param widget Добавляемый виджет
-    void add(Widget &widget) { widgets.push_back(&widget); }
+    void addWidget(Widget &widget) { widgets.push_back(&widget); }
 
     /// @brief Связывание страниц
     /// @details Добавляет виджеты перехода к страницам
     /// @param other Связываемая страница
     void link(Page &other) {
-        this->add(other.to_this);
-        other.add(this->to_this);
+        this->addWidget(other.to_this);
+        other.addWidget(this->to_this);
     }
 
     /// @brief Отобразить страницу
     /// @param render Система отрисовки
     /// @param rows Кол-во строк
-    void render(Render &render, int rows) {
-        render.print(title);
-        render.write('\n');
-
-        rows -= 1;
-
-        const auto start = (totalWidgets() > rows) ? std::min(cursor, totalWidgets() - rows) : 0;
-        const auto end = std::min(start + rows, totalWidgets());
-
-        for (int i = start; i < end; i += 1) {
-            widgets[i]->render(render, i == cursor);
-            render.write('\n');
-        }
-    }
+    void render(Render &render, int rows);
 
     /// @brief Отреагировать на событие
     /// @param event Входящее событие
-    /// @returns true - Требуется перерисовка; false - перерисовка не требуется
-    bool onEvent(Event event) {
-        switch (event) {
-            case Event::None: {
-                return false;
-            }
-            case Event::Update: {
-                return true;
-            }
-            case Event::ElementNext: {
-                moveCursor(+1);
-                return true;
-            }
-            case Event::ElementPrevious: {
-                moveCursor(-1);
-                return true;
-            }
-            case Event::Click: {
-                return widgets[cursor]->onClick();
-            }
-            case Event::ChangeIncrement: {
-                return widgets[cursor]->onChange(+1);
-            }
-            case Event::ChangeDecrement: {
-                return widgets[cursor]->onChange(-1);
-            }
-        }
-
-        return false;
-    }
+    /// @returns true - Требуется перерисовка
+    /// @returns false - перерисовка не требуется
+    bool onEvent(Event event);
 
 private:
     void moveCursor(int delta) {
